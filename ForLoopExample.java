6bd57a682df727f259577929b0a2cea7f37404e4
mihir0702 <mihir.16941@sakec.ac.in>
2024-08-08 11:15:43 +0530

    Add files via upload

---
+++ ForLoopExample.java
@@ -0,0 +1,8 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // For loop syntax: for (initialization; condition; update)
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
